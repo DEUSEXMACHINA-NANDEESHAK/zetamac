9e060b6d5ae42aea7369a24a9a3ec38180040234
Nandeesha K <Nandeesha.K@pens.com>
2025-12-31 03:15:45 +0530

    zetamac: Cors true for backend

--- backend/main.py
+++ backend/main.py
@@ -9,10 +9,10 @@ app = FastAPI(title="Zetamac API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
+    allow_origins=["*"
         # "http://localhost:5173",          # local dev
-        "https://nandeeshak.com",          # prod
-        "https://www.nandeeshak.com",      # prod
+        # "https://nandeeshak.com",          # prod
+        # "https://www.nandeeshak.com",      # prod
     ],
     allow_credentials=True,
     allow_methods=["*"],
